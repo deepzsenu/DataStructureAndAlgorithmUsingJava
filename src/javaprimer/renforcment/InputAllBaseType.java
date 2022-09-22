17356b5f4453549b96af9d1662269c732da59610
deepzsenu <deepzsenu@gmail.com>
2022-09-22 07:44:56 +0530

    first commit

---
+++ src/javaprimer/renforcment/InputAllBaseType.java
@@ -0,0 +1,22 @@
+package javaprimer.renforcment;
+import java.util.*;
+import java.io.*;
+/*
+* R-1.1 Write a short Java method, inputAllBaseTypes, that inputs a different value of
+each base type from the standard input device and prints it back to the standard
+output device.
+*
+* */
+public class InputAllBaseType {
+    public static void inputAllBaseTypes(){
+
+    }
+
+    public static void main(String[] args) {
+        inputAllBaseTypes();
+
+    }
+
+
+
+}
